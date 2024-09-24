a1842d6955dabf36840f692d18b38d8186b58531
Geovane Silva <geovanesilva.hr@gmail.com>
2024-09-24 13:49:37 -0300

    feat: Organiza o projeto inicial

--- projetoTSMX/settings.py
+++ projetoTSMX/settings.py
@@ -79,9 +79,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT =  BASE_DIR / "static_cdn"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- projetoTSMX/urls.py
+++ projetoTSMX/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("livrariaOnline.urls")),
+    path('livrariaOnline/', include("livrariaOnline.urls"))
 ]
