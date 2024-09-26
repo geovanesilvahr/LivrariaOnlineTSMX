b2f3736442b1fe03b031cc29a93801da1d1b6103
Geovane Silva <geovanesilva.hr@gmail.com>
2024-09-26 17:45:04 -0300

    fix: Adiciona os Models principais

--- livrariaOnline/models.py
+++ livrariaOnline/models.py
@@ -1,3 +1,32 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    catergory = models.CharField(max_length=90)
+    date_publication = models.DateField()
+    front_book = models.ImageField()
+    synopsis = models.TextField()
+    pages = models.IntegerField()
+    publisher = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.title
+
+class User(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return self.name
+
+class PurchasesMade(models.Model):
+    id_user = models.ForeignKey("User", on_delete=models.CASCADE)
+    books_list = models.TextField()
+
+    def __str__(self) -> str:
+        return self.id_user
--- projetoTSMX/urls.py
+++ projetoTSMX/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('livrariaOnline/', include("livrariaOnline.urls"))
+    path('', include("livrariaOnline.urls"))
 ]
